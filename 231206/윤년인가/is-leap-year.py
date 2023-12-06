3c06d5c91b4ad8389910f7359ff063cf7ac98c0e
mintenger <68724184+mintenger@users.noreply.github.com>
2023-12-06 10:01:31 +0900

    Title: 윤년인가, Time: 115ms, Memory: 25MB, Status: Passed - CodeTree

--- 231206/윤년인가/is-leap-year.py
+++ 231206/윤년인가/is-leap-year.py
@@ -1,6 +1,6 @@
 year = int(input())
 
-if (year % 4 == 0) or (year % 4 == 0 and year % 400 == 0):
+if (year % 4 == 0 and year % 100 != 0) or (year % 4 == 0 and year % 400 == 0):
     print("true")
 else:
     print("false")
